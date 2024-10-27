6e3eca6d28f42f711a5ff67aeafbe9f34ae60471
Nikunj2003 <njkhitha2003@gmail.com>
2024-10-27 20:52:20 +0530

    Fix: optimized Skill section

--- src/components/skills/skills-showcase.tsx
+++ src/components/skills/skills-showcase.tsx
@@ -1,8 +1,5 @@
 import { AnimatePresence } from "framer-motion";
-
-import SkillPill, {
-  type SkillPillProps,
-} from "@/components/skills/skills-pill";
+import SkillPill, { type SkillPillProps } from "@/components/skills/skills-pill";
 import FadeRight from "@/animation/fade-right";
 import { useScreenBreakpoint } from "@/hooks/useScreenBreakpoint";
 import { useDebounceValue } from "@/hooks/useDebounceValue";
@@ -16,14 +13,12 @@ export interface SkillsShowcaseProps {
 
 export default function SkillsShowcase({ skills }: SkillsShowcaseProps) {
   const isMobile = useScreenBreakpoint(640);
-  const isMobileDebounced = useDebounceValue(isMobile, 300); // reduced debounce time
+  const isMobileDebounced = useDebounceValue(isMobile, 300); // Adjust debounce time
 
   return (
     <section className="overflow-hidden px-6 py-32 sm:px-14 md:px-20">
       <div className="relative mx-auto max-w-7xl">
-        <h2 className="text-xl font-semibold text-accent sm:text-4xl">
-          Skills
-        </h2>
+        <h2 className="text-xl font-semibold text-accent sm:text-4xl">Skills</h2>
         {skills.map((section) => (
           <div key={section.sectionName} className="mt-4">
             <span className="text-xs font-semibold text-foreground sm:text-sm">
@@ -31,14 +26,16 @@ export default function SkillsShowcase({ skills }: SkillsShowcaseProps) {
             </span>
             <div className="mt-2 flex flex-wrap gap-4 text-xl text-accent-foreground">
               {section.skills.map((pill, index) => (
-                <FadeRight
-                  key={`lang-${index}`}
-                  duration={0.4}
-                  delay={0.1 + index * 0.1}
-                  whileInView={!isMobileDebounced} // adjust for mobile
-                >
-                  <SkillPill {...pill} />
-                </FadeRight>
+                <AnimatePresence key={`pill-${index}`}>
+                  <FadeRight
+                    key={`lang-${index}`}
+                    duration={isMobileDebounced ? 0.2 : 0.4} // Reduce duration for smoother mobile
+                    delay={0.1 + index * 0.1}
+                    whileInView={!isMobileDebounced} // Triggers on scroll for non-mobile
+                  >
+                    <SkillPill {...pill} />
+                  </FadeRight>
+                </AnimatePresence>
               ))}
             </div>
           </div>
